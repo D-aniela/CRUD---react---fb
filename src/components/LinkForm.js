89f74da89a79aac99b910d74ad6f89ded872188d
D-aniela <daniela.es.ib@gmail.com>
2021-04-08 16:39:31 -0500

    Crud react

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
 
 // useState, crear el estado de la aplicación
 // El estado son datos que pertenecen a un componente
@@ -19,13 +21,43 @@ const LinkForm = (props) => {
     setValues({ ...values, [name]: value });
   };
 
+  const validateURL = (str) => {
+    return /^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/i.test(
+      str
+    );
+  };
+
   // Crear una función para controlar el formulario
   //handleSubmit recibe información del evento
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!validateURL(values.url)) {
+      return toast("Invalid URL", {
+        type: "warning",
+        autoClose: 1000,
+      });
+    }
+
     props.addOrEditLink(values);
     setValues({ ...initialStateValues });
   };
+
+  // Petición a firebase
+  const getLinkById = async (id) => {
+    const doc = await db.collection("links").doc(id).get();
+    setValues({ ...doc.data() });
+  };
+
+  // Editar link
+  useEffect(() => {
+    if (props.currentId === "") {
+      setValues({ ...initialStateValues });
+    } else {
+      getLinkById(props.currentId);
+      console.log(props.currentId);
+    }
+  }, [props.currentId]);
 
   return (
     <form className="card card-body" onSubmit={handleSubmit}>
@@ -68,7 +100,9 @@ const LinkForm = (props) => {
         ></textarea>
       </div>
 
-      <button className="btn btn-primary btn-block">Save</button>
+      <button className="btn btn-primary btn-block">
+        {props.currentId === "" ? "Save" : "Update"}
+      </button>
     </form>
   );
 };
--- src/components/Links.js
+++ src/components/Links.js
@@ -3,23 +3,42 @@ import React, { useEffect, useState } from "react";
 // el componente es combinado
 
 import LinkForm from "./LinkForm";
+import { toast } from "react-toastify";
 
 import { db } from "../firebase";
 
 const Links = () => {
   const [links, setLinks] = useState([]);
+  const [currentId, setCurrentId] = useState("");
 
   // Función con evento síncrono
   const addOrEditLink = async (linkObject) => {
     // Mientras se guardan los datos queremos que se vaya realizando otro código
-    await db.collection("links").doc().set(linkObject);
-    console.log("New task added");
+    try {
+      if (currentId === "") {
+        await db.collection("links").doc().set(linkObject);
+        toast("New link added", {
+          type: "success",
+        });
+      } else {
+        db.collection("links").doc(currentId).update(linkObject);
+        toast("Link Updated", {
+          type: "info",
+        });
+        setCurrentId("");
+      }
+    } catch (error) {
+      console.error(console.error);
+    }
   };
 
   const onDeleteLink = async (id) => {
     if (window.confirm("Are you sure you want to delete this link?")) {
       await db.collection("links").doc(id).delete();
-      console.log("task deleted");
+      toast("Link deleted successfully", {
+        type: "error",
+        autoClose: 2000,
+      });
     }
   };
 
@@ -47,7 +66,7 @@ const Links = () => {
   return (
     <div>
       <div className="col-md-4 p-2">
-        <LinkForm addOrEditLink={addOrEditLink} />
+        <LinkForm {...{ addOrEditLink, currentId, links }} />
       </div>
       <div className="col-md-8 p-2">
         {links.map((link) => (
@@ -55,12 +74,21 @@ const Links = () => {
             <div className="card-body">
               <div className="d-flex justify-content-between">
                 <h4>{link.name}</h4>
-                <i
-                  className="material-icons text-danger"
-                  onClick={() => onDeleteLink(link.id)}
-                >
-                  close
-                </i>
+                <div>
+                  <i
+                    className="material-icons text-danger"
+                    onClick={() => onDeleteLink(link.id)}
+                  >
+                    close
+                  </i>
+
+                  <i
+                    className="material-icons"
+                    onClick={() => setCurrentId(link.id)}
+                  >
+                    create
+                  </i>
+                </div>
               </div>
               <p>{link.description}</p>
               <a href={link.url} target="_blank" rel="noreferrer">
